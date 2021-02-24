52daebdb132b1078bd40e70610b2a249da6ec4fd
Volkov85 <xplz2007@gmail.com>
2021-02-24 10:06:58 +0600

    Создал компонент Табы

--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import MoviePageOverview from '../movie-page-overview/movie-page-overview';
+import MovieTabs from '../movie-tabs/movie-tabs';
 
 const MoviePage = () => {
   return (
@@ -63,22 +64,8 @@ const MoviePage = () => {
             </div>
 
             <div className="movie-card__desc">
-              <nav className="movie-nav movie-card__nav">
-                <ul className="movie-nav__list">
-                  <li className="movie-nav__item movie-nav__item--active">
-                    <a href="#" className="movie-nav__link">Overview</a>
-                  </li>
-                  <li className="movie-nav__item">
-                    <a href="#" className="movie-nav__link">Details</a>
-                  </li>
-                  <li className="movie-nav__item">
-                    <a href="#" className="movie-nav__link">Reviews</a>
-                  </li>
-                </ul>
-              </nav>
-
+              <MovieTabs />
               <MoviePageOverview />
-
             </div>
           </div>
         </div>
