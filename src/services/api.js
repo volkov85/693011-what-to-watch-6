977019399cc7a3b6dd499d5b9338ea54c991c6ba
Artyom <44151696+volkov85@users.noreply.github.com>
2026-04-10 17:07:08 +0500

    Update BACKEND_URL to new API endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = `https://6.react.pages.academy/wtw`;
+const BACKEND_URL = `https://13.design.htmlacademy.pro/wtw`;
 const REQUEST_TIMEOUT = 5000;
 
 const HttpCode = {
